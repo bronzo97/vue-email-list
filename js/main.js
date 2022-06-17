73b6371bc47a148a9af822c02ba59da4f9110632
bronzo97 <danilobronzo97@hotmail.it>
2022-06-17 19:12:51 +0200

    ...

--- js/main.js
+++ js/main.js
@@ -4,11 +4,25 @@ el: "#app",
 
 
     data: {
-
+        emailGenerated: '',
     },
 	
 
 	methods: {
 
-	}
+        questionCicle() {
+            this.emailGenerated = [];
+            for (let i = 0; i < 10; i++) {
+                getAxiosResponse();
+            }
+        },
+
+        getAxiosResponse() {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((axiosResp) => {
+            this.emailGenerated = axiosResp.data.response;
+            console.log(this.emailGenerated);
+        });
+        },
+    },
 });
